e7a20a1f3f7b9394a24d24e060578c5d08f87c1e
Lucas Souza <lucassouza6932@gmail.com>
2022-12-19 09:55:17 -0300

    Adicionando estilização aos componentes de entrada

--- src/components/InputArea/index.tsx
+++ src/components/InputArea/index.tsx
@@ -14,39 +14,64 @@ export const InputArea =({onAdd}: Props) => {
     const [dateField, setDateField] = useState('')
     const [categoryField, setCategoryField] = useState('')
     const [titleField, setTitleField] = useState('')
-    const [valueField, setValueField] = useState(0)
+    const [valueField, setValueField] = useState('')
 
     const handleAddEvent = () => {
         let newItem: Item = {
             date: new Date(dateField),
             category:  categoryField,
             title: titleField,
-            value: valueField,
+            value: parseFloat(valueField),
         };
         onAdd(newItem);
+    }
+
+    const inputValidation = () => {
+        if (dateField == ''){
+            alert('O Campo DATA precisa ser preenchido!')
+        }
+        if (categoryField == ''){
+            alert('Você precisa especificar uma categoria!')
+        }
+        if (titleField == ''){
+            alert('Você precisa especificar um titulo!')
+        }
+        if (valueField == ''){
+            alert('Você precisa especificar um valor!')
+        }
+        else {
+            handleAddEvent();
+        }
     }
 
     return(
 
         <C.Container>
-                Data:
-                <input type="date" value={dateField} onChange={e => setDateField(e.target.value)}/>
+                <label htmlFor="inputDate">
+                    Data: <br />
+                    <input type="date" value={dateField} onChange={e => setDateField(e.target.value)}/>
+                </label>
                 
-                Categoria:
-                <select name="" id="select" required value={categoryField} onChange={e => setCategoryField(e.target.value)}> 
-                    <option disabled selected></option>
-                    {objectTitles.map((key, index) => (
-                        <option value={key} key={index}>{categories[key].title}</option>
-                    ))}
-                </select>
-
-                Titulo:
-                <input type="text" id='title' value={titleField} onChange={e => setTitleField(e.target.value)}/>
-                
-                Valor:
-                <input type="number" value={valueField} onChange={e => (setValueField(parseFloat(e.target.value)))} />
-
-                <button type="submit" onClick={handleAddEvent}> Adicionar </button>
+                <label htmlFor="select">
+                    Categoria: <br />
+                    <select name="" id="select" required value={categoryField} onChange={e => setCategoryField(e.target.value)}> 
+                        <option disabled selected></option>
+                        {objectTitles.map((key, index) => (
+                            <option value={key} key={index}>{categories[key].title}</option>
+                        ))}
+                    </select>
+                </label>
+                <label htmlFor="inputTitle">
+                    Titulo: <br />
+                    <input type="text" id='title' value={titleField} onChange={e => setTitleField(e.target.value)}/>
+                </label>           
+                <label htmlFor="inputValue">
+                    Valor: <br />
+                    <input type="number" id='value' value={valueField} onChange={e => (setValueField((e.target.value)))} />
+                </label>
+                <label htmlFor=""> <br />
+                    <button className='submit submit2' type="submit" onClick={inputValidation}> Adicionar </button>
+                </label>        
         </C.Container>
     );
 }
--- src/components/InputArea/styles.ts
+++ src/components/InputArea/styles.ts
@@ -7,4 +7,46 @@ export const Container = styled.div`
     padding: 20px;
     margin-top: 20px;
     display: flex;
+    justify-content: space-evenly;
+    font-size: 15px;
+    font-weight: 600;
+
+    #title{
+        width: 12em;
+    }
+
+    #value{
+        width: 10em;
+    }
+    
+    #select{
+        font-size:15px;
+        text-align: justify;
+    }
+
+    .submit{
+        padding: 0.3rem 1rem;
+        font-family: "Poppins", sans-serif;
+        font-weight: 600;
+        font-size: 17px;
+        text-align: center;
+        text-decoration: none;
+        color: #000;
+        border: 1px solid #000;
+        border-radius: 1rem;
+        cursor: pointer;
+        background-color: #fff;
+        position: relative;
+        bottom: 6px;
+    }
+
+    .submit2{
+        border-color: #000;
+        transition: transform 0.2s cubic-bezier(0.235, 0, 0.05, 0.95);
+    
+        :hover{
+            transform: perspective(1px) scale3d(1.044, 1.044, 1) translateZ(0) !important;
+    }
+}
+
 `;
